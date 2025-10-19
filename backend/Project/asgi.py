e53cd9cf8ca65fdd6a2e3986f5cd90c52be5dcf0
Arshdeep Palial <110485426+Arsh910@users.noreply.github.com>
2025-10-19 13:47:04 +0530

    Add files via upload

---
+++ backend/Project/asgi.py
@@ -0,0 +1,27 @@
+import os
+import django
+from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Project.settings')
+
+# Initialize Django and set up the app registry
+django.setup()  # This ensures that the apps are fully loaded before proceeding
+
+# Now it's safe to import middleware and other Django-dependent modules
+from api.middleware import JWTAuthMiddleware
+from videochat import routings as video_routing
+
+django_asgi_app = get_asgi_application()
+
+application = ProtocolTypeRouter({
+    "http": django_asgi_app,
+    "websocket": AllowedHostsOriginValidator(
+        JWTAuthMiddleware(
+            URLRouter(
+                video_routing.websocket_urlpatterns
+            )
+        )
+    ),
+})
