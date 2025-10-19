649ed9fe99ada92b9f17287441de7e5a761320da
Arshdeep Palial <110485426+Arsh910@users.noreply.github.com>
2025-10-19 17:20:25 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,8 +2,14 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  server : {historyApiFallback:true,},
+  server: {
+    middlewareMode: false,
+  },
+  preview: {
+    open: true,
+    port: 5173,
+    historyApiFallback: true,
+  },
 })
