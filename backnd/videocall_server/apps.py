87f8d687401b34df3954fac3cb8647a729a57c77
Arshdeep Palial <110485426+Arsh910@users.noreply.github.com>
2025-10-20 13:35:22 +0530

    Delete backnd directory

--- backnd/videocall_server/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class VideocallServerConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'videocall_server'
--- backnd/videocall_server/routings.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .consumers import VideoRoomConsumer
-
-websocket_urlpatterns=[
-    path('ws/video_chat', VideoRoomConsumer.as_asgi()),
-]
