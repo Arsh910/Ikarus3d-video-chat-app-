baa9e3eba436d962be96c2f25942bcf91c98f601
Arshdeep Palial <110485426+Arsh910@users.noreply.github.com>
2025-10-20 13:41:37 +0530

    Update constants.js

--- frontend/src/config/constants.js
+++ frontend/src/config/constants.js
@@ -1,2 +1,2 @@
-export const SIGNALING_URL = "wss://ikarus3d-video-chat-app-g2xi.onrender.com/ws/video_chat";
+export const SIGNALING_URL = "wss://ikarus3d-video-chatapp-api.onrender.com/ws/video_chat";
 export const STUN = { iceServers: [{ urls: "stun:stun.l.google.com:19302" }] };
