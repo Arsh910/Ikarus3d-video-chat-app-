d150450926822c370c46c6fc0acf8b6f9dd9dbf2
Arsh910 <arshdeeppalial@gmail.com>
2025-10-21 13:26:11 +0530

    hello

--- frontend/src/config/constants.js
+++ frontend/src/config/constants.js
@@ -1,5 +1,5 @@
 
-let HOST = import.meta.env.SERVER_HOST ? `wss://${import.meta.env.SERVER_HOST}` : 'ws://127.0.0.1:8000'
+let HOST = import.meta.env.VITE_SERVER_HOST ? `wss://${import.meta.env.VITE_SERVER_HOST}` : 'ws://127.0.0.1:8000'
 
 export const SIGNALING_URL = HOST+'/ws/video_chat';
 export const STUN = { iceServers: [{ urls: "stun:stun.l.google.com:19302" }] };
